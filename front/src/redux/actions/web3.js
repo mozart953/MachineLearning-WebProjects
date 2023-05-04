ba4c260fecae23711e8674dcf7a1ebefd73a4912
mozart953 <wilsonnianopop@gmail.com>
2023-05-03 19:13:15 -0600

    logic changes

--- front/src/redux/actions/web3.js
+++ front/src/redux/actions/web3.js
@@ -11,3 +11,12 @@ import {
 } from './types'
 
 import {ethers} from "ethers";
+
+export const loadWeb3 = () => async dispatch =>{
+    if(window.ethereum){
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+        }); 
+        localStorage.setItem("account", account[0]);
+    }
+}
